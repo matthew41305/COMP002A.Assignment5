fdff126385c6fcb65a06cc466e11fd249c68e546
Matthew <matthew41305@gmail.com>
2024-11-10 22:41:02 -0800

    functions for sum and range complete, with correct outputs.

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 // 1. Write a range function that takes two arguments, start and end, and returns an array
 // containing all the numbers from start up to (and including) end.
+function range(start, end) {
+    let result = [];
+    for (let i = start; i <= end; i++) {
+        result.push(i);
+    }
+    return result;
+}
+
 // Example output:
 // console.log(range(1, 10)); // outputs [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // 2. Next, write a sum function that takes an array of numbers and returns the sum of
 // these numbers. Run the example program and see whether it does indeed return 55.
 // Example output:
+function sum(numbers) {
+    let total = 0;
+    for (let num of numbers) {
+        total += num;
+    }
+    return total;
 // console.log(sum(range(1, 10))); // returns 55
 
 // 3. Arrays have a reverse method that changes the array by inverting the order in which
